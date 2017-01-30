74fb79353259a93ff94cf33dfc81686ce3a9a794
Adam Luxton <adze_202@hotmail.com>
2017-01-30 18:22:23 -0500

    Finished animated nav.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 //variables
 var logoSVG;
 var clicked = false;
+var navLinks = document.querySelectorAll("nav ul li");
+var linkContainer = document.querySelector("nav ul");
 
 //functions
 function loadSVG(e){
@@ -28,11 +30,15 @@ function resetHamburger(){
 
 function hamburgerClick(){
   document.querySelector('#hamburgerIcon').removeEventListener('click', hamburgerClick, false);
-  if (clicked == false){
+  if (clicked === false){
     //console.log("running");
     TweenMax.to(document.querySelector("#line1"), 0.5, {attr:{x1:716.3, y1:283.7, x2:283.7, y2: 716.3}, ease:Power1.easeOut});
     TweenMax.to(document.querySelector("#line2"), 0.5, {attr:{x1:525, y1:500.1, x2:525, y2: 500.1}, ease:Power1.easeOut});
     TweenMax.to(document.querySelector("#line3"), 0.5, {attr:{x1:716.3, y1:716.4, x2:283.7, y2:283.8}, ease:Power1.easeOut});
+    TweenMax.to(linkContainer, 0.5,{scaleX:1, transformOrigin:"left center", ease:Power1.easeOut});
+    [].forEach.call(navLinks, function(link){
+      TweenMax.to(link, 0.5, {opacity:1, ease:Power1.easeOut, delay:0.5});
+    });
     clicked = true;
     resetHamburger();
   }
@@ -40,6 +46,10 @@ function hamburgerClick(){
     TweenMax.to(document.querySelector("#line1"), 0.5, {attr:{x1:805.9, y1:186.6, x2:194.1, y2: 186.6}, ease:Power1.easeOut});
     TweenMax.to(document.querySelector("#line2"), 0.5, {attr:{x1:805.9, y1:500.1, x2:194.1, y2: 500.1}, ease:Power1.easeOut});
     TweenMax.to(document.querySelector("#line3"), 0.5, {attr:{x1:805.9, y1:813.6, x2:194.1, y2:813.6}, ease:Power1.easeOut});
+    [].forEach.call(navLinks, function(link){
+      TweenMax.to(link, 0.5, {opacity:0, ease:Power1.easeOut});
+    });
+    TweenMax.to(linkContainer, 0.5,{scaleX:0, transformOrigin:"left center", ease:Power1.easeOut, delay: 0.5});
     resetHamburger();
 
     clicked = false;
